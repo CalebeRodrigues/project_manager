467ec54cf569f9d6887d48a7a12219b1fbc38d21
Calebe Rodrigues <calebebrodrigues@gmail.com>
2023-07-09 22:57:21 -0300

    UserModel (Estrutura)

--- backend/src/models/UserModel.js
+++ backend/src/models/UserModel.js
@@ -2,6 +2,25 @@ const { Sequelize } = require('sequelize');
 
 const database = require('../../db');
 
-const UserModel = database.define('user', {});
+const UserModel = database.define('user', {
+  id: {
+    primaryKey: true,
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false
+  },
+  nome: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  senha: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  }
+});
 
 
