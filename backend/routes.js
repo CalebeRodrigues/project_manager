4e51356e57c2b83f43c272eed977f32e1a657de8
Calebe Rodrigues <calebebrodrigues@gmail.com>
2023-07-12 23:40:59 -0300

    Rotas - User

--- backend/routes.js
+++ backend/routes.js
@@ -1,8 +1,22 @@
 const express= require('express');
 const route = express.Router();
 
+const UserController = require('./src/controllers/UserController');
+
 route.get('/', (req, res) => {
   res.status(200).send('API for project TCC (Manager project)');
 });
+
+// User
+route.post('/user/login', UserController.login);
+
+route.get('/user', UserController.findAllUsers);
+route.get('/user/:id', UserController.findUserById);
+
+route.post('/user/register', UserController.register);
+
+route.put('/user/update/:id', UserController.update);
+
+route.delete('/user/delete/:id', UserController.delete);
 
 module.exports = route;
