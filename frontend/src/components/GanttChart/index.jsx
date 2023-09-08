d8dc76665e8afe6b4d6b92614541ec20569f93ed
Calebe Rodrigues <calebebrodrigues@gmail.com>
2023-09-07 23:11:31 -0300

    Adicionando mais pessoas no gráfico de gantt

--- frontend/src/components/GanttChart/index.jsx
+++ frontend/src/components/GanttChart/index.jsx
@@ -13,10 +13,17 @@ const columns = [
 ];
 
 const rows = [
-  ['1', 'Calebe ', 'Calebe', new Date(2023, 11, 27), new Date(2023, 11, 28), null, 100, null],
-  ['2', 'Gusta ', 'Gusta2', new Date(2023, 11, 29), new Date(2023, 11, 30), null, 100, '1'],
-  ['3', 'Jorge ', 'Jorge2', new Date(2023, 12, 1), new Date(2023, 12, 2), null, 100, '1'],
-  ['4', 'Lucas ', 'Lucas2', new Date(2023, 12, 2), new Date(2023, 12, 3), null, 100, null],
+  ['1', 'Calebe', 'Calebe', new Date(2023, 11, 27), new Date(2023, 11, 28), null, 100, null],
+  ['2', 'Gusta', 'Gusta', new Date(2023, 11, 29), new Date(2023, 11, 31), null, 100, '1'],
+  ['3', 'Jorge', 'Jorge', new Date(2023, 12, 1), new Date(2023, 12, 4), null, 100, '1'],
+  ['4', 'Lucas', 'Lucas', new Date(2023, 12, 2), new Date(2023, 12, 3), null, 100, null],
+  ['5', 'Gusta', 'Lucas', new Date(2023, 12, 3), new Date(2023, 12, 4), null, 100, '2'],
+  ['6', 'Calebe', 'Calebe', new Date(2023, 12, 4), new Date(2023, 12, 7), null, 100, '2'],
+  ['7', 'Lucas', 'Lucas', new Date(2023, 12, 5), new Date(2023, 12, 8), null, 100, '1'],
+  ['8', 'Jorge', 'Jorge', new Date(2023, 12, 6), new Date(2023, 12, 10), null, 100, '5'],
+  ['9', 'Lucas', 'Lucas', new Date(2023, 12, 7), new Date(2023, 12, 8), null, 100, null],
+  ['10', 'Gusta', 'Gusta', new Date(2023, 12, 2), new Date(2023, 12, 9), null, 100, null],
+  ['11', 'Calebe', 'Calebe', new Date(2023, 12, 4), new Date(2023, 12, 10), null, 100, null],
 ];
 
 const data = [columns, ...rows];
@@ -31,7 +38,7 @@ const options = {
 
 export const GanttChart = () => {
   return (
-    <div style={{ border: '1px solid red' }}>
+    <div>
       <Chart chartType="Gantt" width="100%" height="50%" data={data} options={options} />
     </div>
   );
