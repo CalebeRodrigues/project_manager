bbff89f0cf8ef544733adedc260a2b383ca57542
Calebe Rodrigues <calebebrodrigues@gmail.com>
2023-10-08 22:50:07 -0300

    Criando novo frontend

---
+++ frontend-alternativo/src/templates/App/index.jsx
@@ -0,0 +1,16 @@
+import './styles.css';
+
+export const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
